f90dfe1ddd8cd6c8bf53ad62a3f37be31ab1ec0a
Yasin Alpay <yalpayelekon@gmail.com>
2025-01-23 22:06:46 +0300

    removed unnecessary file

--- RCU_API_extended.py
+++ RCU_API_extended.py
@@ -4,8 +4,8 @@ from rcu_lib_module.RCU_API import RCU_API
 class RCU_API_Extended(RCU_API):
     @staticmethod
     def FB_device_GTIN(obj: RCU_MessageStructure, data: bytes):
-        RCU_API._RCU_API__SetQUERY(obj)  # Access private method
-        RCU_API._RCU_API__SetGeneral(obj)  # Access private method
+        RCU_API._RCU_API__SetQUERY(obj)  
+        RCU_API._RCU_API__SetGeneral(obj)  
         obj.sub_cmd_no = RCU_MessageStructureConstants.Sub_CMD_No.Query.General.FB_GTIN
         obj.data = data
 
